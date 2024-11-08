a100a703690dd7fe96ea9980316d198e988b7b31
Achudan-Bose <149948198+Achudan-Bose@users.noreply.github.com>
2024-11-08 11:06:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello! Server running, made changes'));
+app.get('/', (req, res) => res.send('Hello! Server running...'));
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
