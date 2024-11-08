76e9e5a32b37e03a0cc15d8bface6cda0c6b8553
Achudan-Bose <achudanbose2@gmail.com>
2024-11-08 10:29:14 +0530

    made changes

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => res.send('Hello! Server running, made changes'));
+
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
